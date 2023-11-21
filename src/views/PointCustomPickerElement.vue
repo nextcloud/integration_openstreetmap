c8e101cf96e2d08f2d785f4ec9a8c170f0bf0d8a
Julien Veyssier <julien-nc@posteo.net>
2023-11-21 11:58:25 +0100

    temporary fix of picker modal height for 28
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/views/PointCustomPickerElement.vue
+++ src/views/PointCustomPickerElement.vue
@@ -264,6 +264,13 @@ export default {
 }
 </script>
 
+<style lang="scss">
+// TODO fix this in nc/vue
+.reference-picker-modal--content {
+	height: 100%;
+}
+</style>
+
 <style scoped lang="scss">
 .point-picker-content {
 	width: 100%;
