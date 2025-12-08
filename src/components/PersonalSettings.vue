4c120aa489af91277f42823c1d73e9ac61af1232
Julien Veyssier <julien-nc@posteo.net>
2025-12-08 13:03:35 +0100

    modernize settings
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/components/PersonalSettings.vue
+++ src/components/PersonalSettings.vue
@@ -5,37 +5,37 @@
 			{{ t('integration_openstreetmap', 'OpenStreetMap integration') }}
 		</h2>
 		<div id="osm-content">
-			<div id="osm-search-block">
-				<NcNoteCard v-if="state.search_location_enabled && state.admin_search_location_enabled"
-					type="info">
-					{{ t('integration_openstreetmap', 'Warning, everything you type in the Unified Search menu will be sent to OpenStreetMap\'s Nominatim service.') }}
-				</NcNoteCard>
-				<NcNoteCard v-if="state.admin_search_location_enabled === false"
-					type="info">
-					{{ t('integration_tmdb', 'An administrator has disabled the OpenStreetMap Unified Search provider') }}
-				</NcNoteCard>
-				<NcCheckboxRadioSwitch
+			<NcNoteCard v-if="state.search_location_enabled && state.admin_search_location_enabled"
+				type="info">
+				{{ t('integration_openstreetmap', 'Warning, everything you type in the Unified Search menu will be sent to OpenStreetMap\'s Nominatim service.') }}
+			</NcNoteCard>
+			<NcNoteCard v-if="state.admin_search_location_enabled === false"
+				type="info">
+				{{ t('integration_tmdb', 'An administrator has disabled the OpenStreetMap Unified Search provider') }}
+			</NcNoteCard>
+			<NcFormBox>
+				<NcFormBoxSwitch
 					:model-value="state.search_location_enabled && state.admin_search_location_enabled"
 					:disabled="!state.admin_search_location_enabled"
 					@update:model-value="onCheckboxChanged($event, 'search_location_enabled')">
 					{{ t('integration_openstreetmap', 'Enable searching for locations') }}
-				</NcCheckboxRadioSwitch>
-				<NcCheckboxRadioSwitch
+				</NcFormBoxSwitch>
+				<NcFormBoxSwitch
 					:model-value="state.link_preview_enabled"
 					@update:model-value="onCheckboxChanged($event, 'link_preview_enabled')">
 					{{ t('integration_openstreetmap', 'Enable OpenStreetMap link previews') }}
-				</NcCheckboxRadioSwitch>
-				<NcCheckboxRadioSwitch v-if="state.link_preview_enabled"
+				</NcFormBoxSwitch>
+				<NcFormBoxSwitch v-if="state.link_preview_enabled"
 					:model-value="state.prefer_simple_osm_iframe"
 					@update:model-value="onCheckboxChanged($event, 'prefer_simple_osm_iframe')">
 					{{ t('integration_openstreetmap', 'Prefer simple OpenStreetMap frame') }}
-				</NcCheckboxRadioSwitch>
-			</div>
-			<NcCheckboxRadioSwitch
-				:model-value="state.navigation_enabled"
-				@update:model-value="onCheckboxChanged($event, 'navigation_enabled')">
-				{{ t('integration_openstreetmap', 'Enable navigation link') }}
-			</NcCheckboxRadioSwitch>
+				</NcFormBoxSwitch>
+				<NcFormBoxSwitch
+					:model-value="state.navigation_enabled"
+					@update:model-value="onCheckboxChanged($event, 'navigation_enabled')">
+					{{ t('integration_openstreetmap', 'Enable navigation link') }}
+				</NcFormBoxSwitch>
+			</NcFormBox>
 		</div>
 	</div>
 </template>
@@ -43,8 +43,9 @@
 <script>
 import OsmIcon from './icons/OsmIcon.vue'
 
-import NcCheckboxRadioSwitch from '@nextcloud/vue/components/NcCheckboxRadioSwitch'
 import NcNoteCard from '@nextcloud/vue/components/NcNoteCard'
+import NcFormBox from '@nextcloud/vue/components/NcFormBox'
+import NcFormBoxSwitch from '@nextcloud/vue/components/NcFormBoxSwitch'
 
 import { loadState } from '@nextcloud/initial-state'
 import { generateUrl } from '@nextcloud/router'
@@ -57,8 +58,9 @@ export default {
 
 	components: {
 		OsmIcon,
-		NcCheckboxRadioSwitch,
 		NcNoteCard,
+		NcFormBox,
+		NcFormBoxSwitch,
 	},
 
 	props: [],
@@ -107,6 +109,10 @@ export default {
 #osm_prefs {
 	#osm-content {
 		margin: 16px 0 0 40px;
+		display: flex;
+		flex-direction: column;
+		gap: 12px;
+		max-width: 800px;
 	}
 	h2 {
 		display: flex;
